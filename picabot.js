36bbcf278822716653ac72204e543576640ad946
pardeepsingh12365 <29483354+pardeepsingh12365@users.noreply.github.com>
2018-01-08 22:38:00 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -463,7 +463,6 @@ var playSong = function(message, connection){
 	var currentSong = songQueue[currentSongIndex];
 	var stream = ytdl(currentSong.url, {"filter": "audioonly"});
 	dispatcher = connection.playStream(stream);
-	volume: 100
 	botChannel.send(`Now ${(shuffle) ? "randomly " : ""}playing \`${currentSong.title}\` :musical_note:, added by ${currentSong.user}`);
 	//bot.user.setGame(currentSong.title);
 	//Workaround since above wouldn't work
