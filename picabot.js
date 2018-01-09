4b4ac44f4a0b60b2450df9d2a856a1572804f705
pardeepsingh12365 <29483354+pardeepsingh12365@users.noreply.github.com>
2018-01-09 14:20:27 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -396,14 +396,11 @@ var playSong = function(message, connection){
 }
 
 var checkForCommand = function(message) {
-  if (!message.author.bot && message.content.startsWith()) {
+  if (!message.author.bot && message.content.startsWith(prefix)) {
     var args = message.content.substring(1).split(' ');
     var command = args.splice(0, 1);
     try {
       commands[command].process(message, args);
-    } catch (e) {
-      message.channel.send("Sorry, that isn't a command yet :sob:", { reply: message });
-      message.channel.send(`You can type \`${prefix}help\` to see a list of my commands`);
     }
   }
 };
