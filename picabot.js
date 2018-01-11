02d40ccbc99358384c6e587032fdff71ea095b74
pardeepsingh12365 <29483354+pardeepsingh12365@users.noreply.github.com>
2018-01-11 15:15:15 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -11,7 +11,7 @@ const botChannelName = "icwbot2";
 var botChannel;
 var fortunes = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely of it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Dont count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
 var dispatcher;
-var songQueue = [];
+const songQueue = new Map();
 var currentSongIndex = 0;
 var previousSongIndex = 0;
 var shuffle = false;
