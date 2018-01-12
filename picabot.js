f9ac5a892c3ca5e736c87065e723fc1d936d54aa
pardeepsingh12365 <29483354+pardeepsingh12365@users.noreply.github.com>
2018-01-12 12:49:40 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -285,17 +285,22 @@ bot.on("message", function(message){
 	}
 
 	if (command === "volume") {
-		if (args[1] < 0 || args[1] > 100) {
-			message.channel.send("Invalid Volume! Please provide a volume from 0 to 100.");
-			return;
+		if(message.member.voiceChannel !== undefined){
+			if (args[1] < 0 || args[1] > 10) {
+				message.channel.send("Invalid Volume! Please provide a volume from 0 to 10.");
+				return;
+			}
+			//volume[message.guild.id] = Number(args[1]) / 100;
+			//server.dispatcher = connection.playStream(YTDL(video.url, { filter: "audioonly" }));	
+			//var server = servers[message.guild.id];
+			//if (serverQueue.dispatcher) {
+			serverQueue.volume = args[1];
+			dispatcher.setVolumeLogarithmic(args[1] / 5);
+			message.channel.send(`Volume set: ${args[1]}%`);
+		
+		}else{
+			message.channel.send("You can't hear my music if you're not in a voice channel :cry:", {reply: message});
 		}
-		//volume[message.guild.id] = Number(args[1]) / 100;
-		//server.dispatcher = connection.playStream(YTDL(video.url, { filter: "audioonly" }));
-		//var server = servers[message.guild.id];
-		//if (serverQueue.dispatcher) {
-		serverQueue.volume = args[1];
-		dispatcher.setVolumeLogarithmic(args[1] / 100);
-		message.channel.send(`Volume set: ${args[1]}%`);
 		//}
 	}
 });
