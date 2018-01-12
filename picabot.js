7c18c8a73b1fffbba5358e76910d03f1fc188728
pardeepsingh12365 <29483354+pardeepsingh12365@users.noreply.github.com>
2018-01-12 13:32:00 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -285,7 +285,7 @@ bot.on("message", function(message){
 	}
 
 	if (command === "volume") {
-		if(bot.voiceChannel !== undefined){
+		//if(bot.voiceChannel !== undefined){
 			if(message.member.voiceChannel !== undefined){
 				if (args[1] < 0 || args[1] > 100) {
 					message.channel.send("Invalid Volume! Please provide a volume from 0 to 10.");
@@ -295,14 +295,14 @@ bot.on("message", function(message){
 				//server.dispatcher = connection.playStream(YTDL(video.url, { filter: "audioonly" }));	
 				//var server = servers[message.guild.id];
 				//if (serverQueue.dispatcher) {
-				serverQueue.volume = args[1];
-				dispatcher.setVolumeLogarithmic(args[1] / 100);
+				volume[message.guild.id] = Number(args[1]) / 100;
+				dispatcher.setVolumeLogarithmic(volume[message.guild.id]);
 				message.channel.send(`Volume set: ${args[1]}%`);
 		
 			}else{
 				message.channel.send("You can't change volume if you're not in a voice channel :cry:", {reply: message});
 			}
-		}else{
+		//}else{
 			message.channel.send("you cant change voice cause bot is not in voice channel", {reply: message});
 		}
 		//}
