03c67d16383942c8798bea7367ddac2ef820cda7
pardeepsingh12365 <29483354+pardeepsingh12365@users.noreply.github.com>
2018-01-13 16:30:32 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -398,20 +398,6 @@ var playSong = function(message, connection){
 		});
 	}
 };
-
-var checkForCommand = function(message) {
-	if (!message.author.bot && message.content.startsWith(prefix)) {
-		var args = message.content.substring(1).split(" ");
-		var command = args.splice(0, 1);
-		try {
-			commands[command].process(message, args);
-		} catch (e) {
-			message.channel.send("error line 404 checkforcommand :cry:", {reply: message});
-		}
-	}
-};
-
-
 function newFunction() {
 	return queue.message.guild.id;
 }
