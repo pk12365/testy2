7b589b06ec492ffcf5dfd6e266e97875ad3ffd8e
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-01-21 01:13:09 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -332,7 +332,7 @@ var addSong = function(message, url) {
 				textChannel: message.channel,
 				connection: null,
 				songs: [],
-				volume: args[1],
+				volume: [],
 				playing: true
 			};
 
