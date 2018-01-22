19db2702f5e2e73840a7611cbc331757a3d0324e
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-01-22 20:25:43 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -306,6 +306,11 @@ bot.on("message", function(message) {
 					}
 				serverQueue.volume[message.guild.id] = args[1];
 				dispatcher.setVolumeLogarithmic(args[1] / 80);
+				var embed = new Discord.RichEmbed()
+					.setTitle("volume controls")
+					.setDescription(`volume set ${args[1]}%`)
+					.setThumbnail("https://images-ext-1.discordapp.net/external/v1EV83IWPZ5tg7b5NJwfZO_drseYr7lSlVjCJ_-PncM/https/cdn.discordapp.com/icons/268683615632621568/168a880bdbc1cb0b0858f969b2247aa3.jpg?width=80&height=80")
+					.setFooter("Changed by: " + message.author.username.toString(), message.author.avatarURL);
 				message.channel.send({embed: setvolembed});
 				//}
 		} else {
@@ -415,7 +420,3 @@ var playSong = function(message, connection) {
 };
 
 var setvolembed = new Discord.RichEmbed()
-.setTitle("volume controls")
-.setDescription(`volume set ${args[1]}%`)
-.setThumbnail("https://images-ext-1.discordapp.net/external/v1EV83IWPZ5tg7b5NJwfZO_drseYr7lSlVjCJ_-PncM/https/cdn.discordapp.com/icons/268683615632621568/168a880bdbc1cb0b0858f969b2247aa3.jpg?width=80&height=80")
-.setFooter("Changed by: " + message.author.username.toString(), message.author.avatarURL);
