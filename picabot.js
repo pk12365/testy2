97facc2e616de5a29446f320237bf0afe966e244
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-01-23 15:21:04 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -310,7 +310,8 @@ bot.on("message", function(message) {
 					.setTitle("volume controls")
 					.setDescription(`volume set ${args[1]}%`)
 					.setThumbnail("https://images-ext-1.discordapp.net/external/v1EV83IWPZ5tg7b5NJwfZO_drseYr7lSlVjCJ_-PncM/https/cdn.discordapp.com/icons/268683615632621568/168a880bdbc1cb0b0858f969b2247aa3.jpg?width=80&height=80")
-					.setFooter("Changed by: " + message.author.username.toString(), message.author.avatarURL);
+					.setFooter("Changed by: " + message.author.username.toString(), message.author.avatarURL)
+					.setTimestamp();
 				message.channel.send({embed: setvolembed});
 				//}
 		} else {
@@ -354,7 +355,7 @@ var addSong = function(message, url) {
 			.setDescription("link here" + url)
 			.setURL(`${url}`)
 			.setThumbnail("https://images-ext-1.discordapp.net/external/v1EV83IWPZ5tg7b5NJwfZO_drseYr7lSlVjCJ_-PncM/https/cdn.discordapp.com/icons/268683615632621568/168a880bdbc1cb0b0858f969b2247aa3.jpg?width=80&height=80")
-			.setFooter("Requested by: " + `${message.author.user}`, message.author.usravatar)
+			.setFooter("Requested by: " + message.author.username.toString(), message.author.avatarURL)
 			.setTimestamp();
 			message.channel.send({embed: addsongembed});
 
