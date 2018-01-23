0197b1ee24b31cef16f0fcd219206e228cf91f0f
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-01-23 13:06:45 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -380,7 +380,7 @@ var playSong = function(message, connection) {
 		var nowplayembed = new Discord.RichEmbed()
 		.setTitle("Now Playing")
 		.setDescription(`Now ${(shuffle) ? "randomly " : ""}playing \`${currentSong.title}\` :musical_note:`)
-		.setThumbnail(currentSong.thumbnails)
+		.setThumbnail("https://images-ext-1.discordapp.net/external/v1EV83IWPZ5tg7b5NJwfZO_drseYr7lSlVjCJ_-PncM/https/cdn.discordapp.com/icons/268683615632621568/168a880bdbc1cb0b0858f969b2247aa3.jpg?width=80&height=80")
 		.setFooter("Added by: " + `${currentSong.user}`);
 		message.channel.send({embed: nowplayembed});
 		//bot.user.setGame(currentSong.title);
