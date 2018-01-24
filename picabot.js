0a037aaa5fd93e8d366d5cc239fd6e184a73ce2a
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-01-24 17:22:37 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -262,13 +262,13 @@ bot.on("message", function(message) {
 					currentSongIndex = 0;
 					serverQueue.songs = [];
 					message.member.voiceChannel.leave();
-					var finishembed = new Discord.RichEmbed()
+					var stopembed = new Discord.RichEmbed()
 						.setColor(0x008000)
 						.setAuthor("Finished playing because no more song in the queue", "https://cdn.discordapp.com/attachments/398789265900830760/405592021579989003/videotogif_2018.01.24_10.46.57.gif")
 						.setDescription("thanks for using see you soon bye bye 👋")
 						.setFooter("Developed by: PK#1650 ", "https://cdn.discordapp.com/attachments/399064303170224131/405585474988802058/videotogif_2018.01.24_10.14.40.gif")
 						.setTimestamp();
-					message.channel.send("Clearing queue and stopping music!");
+					message.channel.send({embed: stopembed});
 				}
 				/*else if(args.length > 0){
 					var index = Number.parseInt(args[0]);
