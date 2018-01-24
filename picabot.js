707c7f17558810e4069f1c9c43fda8fc82a9430c
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-01-24 15:52:22 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -330,7 +330,7 @@ bot.on("message", function(message) {
 					}
 				}
 				var queueembed = new Discord.RichEmbed()
-				.setAuthor(`${message.guild.iconURL}` + "The song queue of " + message.guild.name + " currently has:")
+				.setAuthor(message.guild.iconURL, "The song queue of " + message.guild.name + " currently has:")
 				.setDescription(`${songList}`)
 				.setFooter("Developed by: PK#1650 ", "https://cdn.discordapp.com/attachments/399064303170224131/405585474988802058/videotogif_2018.01.24_10.14.40.gif")
 				message.channel.send({embed: queueembed});
