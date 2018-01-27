c273d8d1f0fc589f9fd79c29d35669488319d240
pk12365 <34957492+pk12365@users.noreply.github.com>
2018-01-27 13:18:13 +0530

    Update picabot.js

--- picabot.js
+++ picabot.js
@@ -16,7 +16,6 @@ var currentSongIndex = 0;
 var previousSongIndex = 0;
 var shuffle = false;
 var autoremove = false;
-const serverQueue = songQueue.get(message.guild.id);
 
 bot.on("ready", function() {
 	console.log("Bot ready");
@@ -55,6 +54,8 @@ fs.readFile("save.json", function(err, data) {
 });
 
 bot.on("message", function(message) {
+	if (!(message.channel instanceof Textchannel)) return undefined;
+	const serverQueue = songQueue.get(message.guild.id);
 
 	if (message.author.bot) return undefined;
 
